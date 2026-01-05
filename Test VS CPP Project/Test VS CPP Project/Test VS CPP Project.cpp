2cf1860dd544ad024884643d1cc466f9df1121a9
Niklas Henderson <niklas.henderson@btinternet.com>
2026-01-05 11:58:44 +0000

    New monster objects

--- Test VS CPP Project/Test VS CPP Project/Test VS CPP Project.cpp
+++ Test VS CPP Project/Test VS CPP Project/Test VS CPP Project.cpp
@@ -1,8 +1,13 @@
 #include <iostream>
+#include "Monster.h"
 
 using namespace std;
 
 int main()
 {
+	Monster greg = Monster("Greg", "Human", 2);
+	Monster monster2 = Monster("wdaw", "Human", 2);
+	Monster stuart = Monster("Stu", "Human", 2);
+
 	
 }
